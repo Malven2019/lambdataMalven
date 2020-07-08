f2e374776c4ae09b8bb738701a38b3cd87d729df
Malven Mtombeni <malmtombeni@gmail.com>
2020-07-08 04:08:07 -0400

    Prepare to distribute our package

---
+++ Setup.py
@@ -0,0 +1,20 @@
+# setup.py file
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="LAMBDATAMALVEN",  # the name that you will install via pip
+    version="1.0",
+    author="Malven Mtombeni",
+    author_email="malmtombeni@gmail.com",
+    description="The Set up file",
+    long_description=long_description,
+    # required if using a md file for long desc
+    long_description_content_type="text/markdown",
+    #license="MIT",
+    url="https://github.com/Malven2019/LambdataMalven",
+    #keywords="",
+    packages=find_packages()  # ["my_lambdata"]
+)
